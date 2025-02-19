99dc874af2e8e3c28cd01b1bda0c42252c44aa84
MatheusGmota <livematgreg@gmail.com>
2025-02-19 20:41:18 -0300

    Primeiro exercício finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
+
+// Excercio 1
+// Crie um array de números e imprima a soma dos valores.
+let lista: number[] = [10 , 2, 8];
+
+let soma: number = lista.reduce((valor, valorAtual) => valor + valorAtual, 0)
+
+console.log(soma);
